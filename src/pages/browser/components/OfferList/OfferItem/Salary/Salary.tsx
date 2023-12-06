cddc4cfb45466592156a56c72e4a6b7d0418ef75
Burit00 <burciak01@gmail.com>
2023-12-06 18:45:53 +0100

    fix: OfferCard

--- src/pages/browser/components/OfferList/OfferItem/Salary/Salary.tsx
+++ src/pages/browser/components/OfferList/OfferItem/Salary/Salary.tsx
@@ -1,20 +1,23 @@
-import { TSalary } from "@api/types/offer.ts";
+import { TSalary } from "@api/types/salary.ts";
 import TextWithIcon from "@components/UI/molecules/TextWithIcon.tsx";
 import classes from "./Salary.module.scss";
+import { SALARY_EMPLOYMENT_MAP } from "@api/maps/salary-employment.ts";
 
 interface ISalaryProps {
   salaryRange: TSalary;
 }
 
 const Salary = (props: ISalaryProps) => {
-  const salary = props.salaryRange.to
-    ? `${props.salaryRange.from} - ${props.salaryRange.to}`
-    : `${props.salaryRange.from}`;
+  const salary = props.salaryRange.salaryTo
+    ? `${props.salaryRange.salaryFrom} - ${props.salaryRange.salaryTo}`
+    : `${props.salaryRange.salaryFrom}`;
+
+  const employmentType = SALARY_EMPLOYMENT_MAP.get(props.salaryRange.employmentType);
 
   return (
     <TextWithIcon iconBefore="attach_money">
       <span>
-        <span className={classes.salary}>{salary}</span> zł
+        <span className={classes.salary}>{salary}</span> zł ({employmentType})
       </span>
     </TextWithIcon>
   );
