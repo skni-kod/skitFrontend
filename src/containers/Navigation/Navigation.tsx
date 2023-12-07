917def98e953f52e874e2dff3fe2f47619fb5915
Burit00 <burciak01@gmail.com>
2023-12-07 01:16:52 +0100

    navigation component added

--- src/containers/Navigation/Navigation.tsx
+++ src/containers/Navigation/Navigation.tsx
@@ -1,5 +1,24 @@
+import logo from 'assets/logo.svg';
+import classes from './Navigation.module.scss';
+import Input from "@components/UI/molecules/Input.tsx";
+import Button from "@components/UI/molecules/Button.tsx";
+import { NavLink } from "react-router-dom";
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+
+  return (
+    <nav className={classes.navigation} >
+      <div className={classes['left-side']}>
+        <NavLink to={"browser"} style={{display: "flex"}}>
+          <img className={classes.logo} src={logo} alt="logo" />
+        </NavLink>
+        <Input iconBefore={"search"} label={"Szukaj pracy w IT"} type={"text"} name={"search"} />
+      </div>
+      <div className={classes['right-side']}>
+        <Button iconBefore={"add"}>Dodaj ogłoszenie</Button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
