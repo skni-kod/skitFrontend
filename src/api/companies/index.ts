094b4bd8fa25b7521ad1f4696aacb21f62b0f6e4
Burit00 <burciak01@gmail.com>
2024-03-01 16:08:23 +0100

    feat: add type to getCompanies response

--- src/api/companies/index.ts
+++ src/api/companies/index.ts
@@ -1,11 +1,13 @@
 import httpClient from "@api/httpClient.ts";
 import { TCompany } from "@shared/types/domain/companies/company.ts";
+import { TPaginator } from "@shared/types/pagination/paginator.ts";
 
 const BASE_URL = "/companies";
 const BASE_PUBLIC_URL = "/public/companies";
 
-export function getCompanies(): Promise<TCompany[]> {
-  return httpClient.get<TCompany[]>(BASE_PUBLIC_URL);
+type TCompaniesResponse = { companies: TPaginator<TCompany>; };
+export function getCompanies(): Promise<TCompaniesResponse> {
+  return httpClient.get<TCompaniesResponse>(BASE_PUBLIC_URL);
 }
 
 export function getCompanyDetailsForUpdateForm() {
