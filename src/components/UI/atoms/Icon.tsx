6e5d8d05bcd12169dd1cd451ae19841b6b77e503
Burit00 <burciak01@gmail.com>
2024-03-09 08:49:48 +0100

    feat: add "size" prop to Icon component

--- src/components/UI/atoms/Icon.tsx
+++ src/components/UI/atoms/Icon.tsx
@@ -4,24 +4,27 @@ import { icons } from "lucide-react";
 export type IconName = keyof typeof icons;
 
 type IconProps = {
-  className?: string;
   iconName: IconName;
+  className?: string;
+  size?: number;
   color?: "primary" | "secondary";
   onClick?: () => void;
 }
 
 const Icon = (props: IconProps) => {
-  const {iconName, color, className, onClick} = props;
+  const {iconName, color, className, ...iconProps} = props;
   const LucideIcon = icons[iconName];
 
   let classNames = classes['icon'];
 
   if (color) classNames += " " + classes[`icon--${color}`];
   if (className) classNames += " " + className;
-  if (onClick) classNames += " pointer";
+  if (iconProps.onClick) classNames += " pointer";
+
+  iconProps.size ??= 20;
 
   return (
-    <LucideIcon size={20} className={classNames} onClick={onClick} />
+    <LucideIcon className={classNames} {...iconProps} />
   );
 };
 
